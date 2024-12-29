aa67d1896cc8cd68ba37da4e93926128424da955
mreza-slt <rsltany649@gmail.com>
2024-12-29 14:59:43 +0330

    feature: delete note in note list

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,19 @@ function App() {
     setNotes((prevNotes) => [...prevNotes, newNote]);
   };
 
+  const handleDeleteNote = (id) => {
+    setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
+  };
+
+  
+
   return (
     <div className="container">
       <div className="note-header">note header</div>
       <div className="note-app">
         <AddNewNote onAddNote={handleNote} />
         <div className="note-container">
+          <NoteList notes={notes} onDelete={handleDeleteNote} />
         </div>
       </div>
     </div>
---
+++ src/components/NoteList.jsx
@@ -0,0 +1,36 @@
+function NoteList({ notes ,onDelete}) {
+  return (
+    <div className="note-list">
+      {notes.map((note) => (
+        <NoteItem key={note.id} note={note} onDelete={onDelete} />
+      ))}
+    </div>
+  );
+}
+
+export default NoteList;
+
+function NoteItem({ note, onDelete }) {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return (
+    <div className="note-item">
+      <div className="note-item__header">
+        <div>
+          <p className="title">{note.title}</p>
+          <p className="desc">{note.description}</p>
+        </div>
+        <div className="actions">
+          <button onClick={()=>onDelete(note.id)}>Delete</button>
+          <input type="checkbox" name="" id="" />
+        </div>
+      </div>
+      <div className="note-item__footer">
+        {new Date(note.createdAt).toLocaleDateString("en-US", options)}
+      </div>
+    </div>
+  );
+}
